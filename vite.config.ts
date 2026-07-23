2a29f5ea1191441a7c28948493812da08950b963
Shashank <ferocious2006@gmail.com>
2026-07-23 23:34:24 +0530

    site xml changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import ViteSitemap from "vite-plugin-sitemap";
 
-// https://vite.dev/config/
 export default defineConfig(async () => {
-  const plugins = [react(), tailwindcss()];
+  const plugins = [
+    react(),
+    tailwindcss(),
+    ViteSitemap({
+      hostname: "https://earnmoneyai.netlify.app",
+    }),
+  ];
+
   try {
     // @ts-ignore
-    const m = await import('./.vite-source-tags.js');
+    const m = await import("./.vite-source-tags.js");
     plugins.push(m.sourceTags());
   } catch {}
-  return { plugins };
-})
+
+  return {
+    plugins,
+  };
+});
